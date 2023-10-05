80c0bedf239291943eede3c8df9c79e50893e255
Caleb <kamenjuhkiiru@gmail.com>
2023-10-05 09:53:40 +0300

    Completed assignment

--- main.js
+++ main.js
@@ -4,7 +4,31 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+mimicServerCall(url="http://mimicServer.example.com", config={})
+  .then(response => {
+    console.log("Success:", response);// used for debugging the success in the console
+    const like = document.querySelector(".like-glyph")
+    like.addEventListener("click", () => {
+      if(like.innerText === EMPTY_HEART) {
+        like.innerText = FULL_HEART
+      } else {
+        like.innerText = EMPTY_HEART
+      }
+    })
 
+    
+  })
+  .catch(error => {
+    console.error("Error:", error); // used for debugging the error in the console
+    const hidden = document.querySelector(".hidden")
+    hidden.classList.remove("hidden")
+    const modal = document.getElementById("modal-message")
+    modal.textContent = error
+    setTimeout(() => {
+      hidden.classList.add("hidden")
+    }, 3000)
+    
+  });
 
 
 //------------------------------------------------------------------------------
